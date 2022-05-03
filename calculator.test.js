122dbd0dc44e0523409ca0327903aa3a94b160ab
Opeyemi <opeadetola@gmail.com>
2022-05-02 21:03:24 -0400

    calculator test suite

---
+++ calculator.test.js
@@ -0,0 +1,16 @@
+import { add, sub, div, mul } from "./calculator";
+
+describe("return correct calculation for each operation", () => {
+  test("add", () => {
+    expect(add(5, 2)).toBe(7);
+  });
+  test("subtract", () => {
+    expect(sub(5, 2)).toBe(3);
+  });
+  test("divide", () => {
+    expect(div(5, 2)).toBe(2.5);
+  });
+  test("multiply", () => {
+    expect(mul(5, 2)).toBe(10);
+  });
+});
